583b6fa375992ad4d75fe2e9473eb061472dd2d5
eunsang.kim <gomuzom@naver.com>
2022-08-02 23:55:04 +0900

    first commit

---
+++ lib/root_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _handleCurrentScreen();
+  }
+
+  Widget _handleCurrentScreen() {
+    return Container();
+  }
+}
