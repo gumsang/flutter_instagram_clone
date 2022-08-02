21a33819d8c515849b676f997c15209b68cbef32
eunsang.kim <gomuzom@naver.com>
2022-08-03 01:56:59 +0900

    feat 파이어베이스 세팅 추가

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Instagram Clone',
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SignInButton(Buttons.Google, onPressed: () {})
+          ],
+        ),
+      ),
+    );
   }
 }
